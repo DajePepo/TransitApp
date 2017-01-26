c90c27399bbe50794611e09cbfd97d1a87569c10
Pietro Santececca <pietro@apps4gaps.com>
2017-01-26 11:25:47 +0100

    add directions content

---
+++ TransitApp/HeaderSectionDirectionView.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderSectionDirectionView.swift
+//  TransitApp
+//
+//  Created by Pietro Santececca on 25/01/17.
+//  Copyright © 2017 Tecnojam. All rights reserved.
+//
+
+import UIKit
+
+class HeaderSectionDirectionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
